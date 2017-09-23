5317da85d7b4bef228dc13dc84e88423843acdb8
James McCafferty <jamesmccafferty@live.ca>
2017-09-23 10:30:57 -0400

    Added More Detectors

---
+++ GeneticGeneration/Assets/Scripts/BehaviourTrees/Detectors/DirectionDetector.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DirectionDetector : IDetector
+{
+	private GameObject m_actor;
+	private GameObject m_target;
+	private Vector2 m_direction;
+	private float m_angle_threshold;
+
+ 
+  public DirectionDetector(GameObject p_actor, GameObject p_target, Vector2 p_direction, float p_angle_threshold)
+  {
+		m_actor = p_actor;
+		m_target = p_target;
+		m_direction = p_direction;
+		m_angle_threshold = p_angle_threshold;
+  }
+
+  public bool detect()
+  {
+    //Vector representing direction actor should be pointing
+		Vector2 target_direction = m_target.transform.position - m_actor.transform.position;
+
+		return Vector2.Angle( target_direction, m_direction ) < m_angle_threshold;
+  }
+}
---
+++ GeneticGeneration/Assets/Scripts/BehaviourTrees/Detectors/ProximityDetector.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Calc;
+
+public class ProximityDetector : IDetector {
+  
+	private GameObject m_actor;
+	private GameObject m_target;
+	private float m_threshold;
+
+	public ProximityDetector(GameObject p_actor, GameObject p_target, float p_threshold){
+		m_actor = p_actor;
+		m_target = p_target;
+		m_threshold = p_threshold;
+	}
+
+	public bool detect()
+  {
+    return (VectorCalc.CalcVec3to2(m_target.transform.position) - VectorCalc.CalcVec3to2(m_actor.transform.position)).magnitude <= m_threshold;
+  }
+}
