cdf89660f5c44e7d5ebca9ebeafe513b03d50459
James McCafferty <jamesmccafferty@live.ca>
2017-10-29 13:00:11 -0400

    Better Matrix Library

---
+++ GeneticGeneration/Assets/Scripts/Test.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MathNet.Numerics.LinearAlgebra;
+
+public class Test : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+		Matrix<float> x = Matrix<float>.Build.Dense(3, 3, (i,j) => { return 2f; } );
+		Matrix<float> y = Matrix<float>.Build.Dense(1,4, (i,j) => { return 2f; } );
+
+		Debug.Log(x.ToString());
+		Debug.Log(y.ToString());
+		//Debug.Log((x*y).ToString());
+
+		Debug.Log(y.RowCount);
+
+		x[1,2] = 4;
+		Debug.Log(x[1,2]);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
